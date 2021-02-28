c016b18b556be2eb9ecc1826c9828fee69f2f992
Monir H <monirhfy@gmail.com>
2021-03-01 00:35:26 +0600

    Stater file

---
+++ src/components/playersData.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    name: 'Messi',
+    price: 100,
+    avatar: 'imag',
+  },
+  {
+    id: 2,
+    name: 'Messi',
+    price: 100,
+    avatar: 'imag',
+  },
+];
